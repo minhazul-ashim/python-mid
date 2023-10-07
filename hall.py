0de8b5f4eded398a0c766524c678cda6f95dcb13
Minhazul Ashim <ashim.ashim7470@gmail.com>
2023-10-07 20:01:29 +0600

    Hopefully project is ready

--- hall.py
+++ hall.py
@@ -39,18 +39,22 @@ class Hall :
 
         if(x >= self.rows or y >= self.cols) :
             print("Invalid Seats");
-        
+            return;
+    
         for show in self.show_list :
             if(show[0] == id) :
                 target_show = self.seats[id];
         
         if(target_show and target_show[x][y] == "Booked") :
             print("Sorry! The Seat is already Booked");
+            return;
 
         if(target_show) :
             target_show[x][y] = 'Booked';
+            print("Your Seats has been booked");
         else :
             print("Sorry, You provided wrong show ID.");
+            return;
         
 
     # Method to view all the shows running in the hall;
@@ -77,43 +81,83 @@ class Hall :
         print(f'Hall ID is {self.hall_no}, with Total Seats {self.rows * self.cols} and shows running {len(self.show_list)}');
 
 
-
-
-
 hall = Star_Cinema.entryHall(5,5,100);
 
-hall.entry_show("101", "Jawan", datetime.datetime.now());
-# hall.view_show_list();
-hall.book_seats("101", (1,1));
-hall.view_available_seats("101");
-hall.book_seats("101", (1,1));
+hall.entry_show("101", "Avenger Endgame", datetime.datetime.now());
+hall.entry_show("102", "Wonder Woman", datetime.datetime.now());
 
 while True :
-    print('Welcome to the Star Cinema.');
-    print("Choose your option.");
-    print("1 : Entry Hall");
-    print("2 : Entry Show");
-    print("3 : View Show List");
-    print("4 : View Available Seat");
-    print("5 : Book Seat");
-    print("6 : Exit");
-    print("Go to ", end=" ");
-    x = int(input());
-    
-    match x :
-        case 1 :
-            print('Entry hall');
-        case 2 :
-            print('Entry Show');
-        case 3 :
-            print('View Showlist');
-        case 4 :
-            print('View Seats');
-        case 5 :
-            print('Book Seat');
-        case 6: 
-            print('Process Terminated');
-            break;
-    
-    
+    print('\n \n');
+    print('Welcome to the Star Cinema Dashboard');
+    print("1 : Interact as User");
+    print("2 : Interact as Counter");
+    print("3 : Terminate Process");
+    n = int(input());
 
+    if(n == 1) :
+
+        while True :
+            
+            print('\n \n Interacting as User');
+            print("Choose your option.");
+            print("1 : View Show List");
+            print("2 : Book Seat");
+            print("3 : Return to Dashboard");
+            print("Go to ", end=" ");
+            x = int(input());
+            
+            match x :
+                case 1 :
+                    hall.view_show_list();
+                case 2 :
+                    print("Enter Show ID", end=" ");
+                    id = input();
+                    print("Enter Row", end=" ");
+                    x = int(input());
+                    print("Enter Column", end = " ");
+                    y = int(input());
+                    hall.book_seats(id, (x, y));
+                case 3: 
+                    print('Process Terminated');
+                    break;
+    elif(n == 2) :
+        
+        print('\n \n Interacting as Counter');
+        print("Choose your option.");
+        print("1 : Entry Show");
+        print("2 : View Show List");
+        print("3 : View Available Seat");
+        print("4 : Book Seat");
+        print("5 : Return to Dashboard");
+        print("Go to ", end=" ");
+        x = int(input());
+        
+        match x :
+            case 1 :
+                print("Enter Movie Name", end=" ");
+                x = input();
+                print("Enter Show ID", end=" ");
+                y = input();
+                hall.entry_show(y, x, datetime.datetime.now());
+                print("Show Entry Successful");
+            case 2 :
+                hall.view_show_list();
+            case 3 :
+                print("Enter Show ID", end=" ");
+                x = input();
+                hall.view_available_seats(x);
+            case 4 :
+                print("Enter Show ID", end=" ");
+                id = input();
+                print("Enter Row", end=" ");
+                x = int(input());
+                print("Enter Column", end = " ");
+                y = int(input());
+                hall.book_seats(id, (x, y));
+            case 5: 
+                print('Process Terminated');
+                break;
+    
+    elif(n == 3) :
+        print("Process Terminated");
+        break;
